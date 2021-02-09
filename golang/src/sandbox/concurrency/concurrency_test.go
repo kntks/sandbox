4978e359491291e75375337e5c6aa80b7e7aa171
kntks <0180takeshi.aefd@gmail.com>
2021-02-09 23:27:59 +0900

    remove: test

--- golang/src/sandbox/concurrency/concurrency_test.go
+++ golang/src/sandbox/concurrency/concurrency_test.go
@@ -2,14 +2,6 @@ package concurrency
 
 import "testing"
 
-func TestCase1(t *testing.T) {
-	Case1()
-}
-
-func TestCase2(t *testing.T) {
-	Case2()
-} 
-
 func BenchmarkCase1(t *testing.B) {
 	for i := 0; i < t.N; i++ {
 		Case1()
