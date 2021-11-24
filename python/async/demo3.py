0c4930cb3a7bbfc60c6da3624eed7791ed993764
kntks <0180takeshi.aefd@gmail.com>
2021-11-24 21:59:12 +0900

    add sample

--- python/async/demo3.py
+++ python/async/demo3.py
@@ -1,11 +1,13 @@
-import asyncio
+import random
 import time
+import asyncio
 from google.cloud import resourcemanager
 from google.cloud import bigquery
 from typing import List
 
 from google.cloud.resourcemanager_v3.services.folders.pagers import ListFoldersAsyncPager
 from google.cloud.resourcemanager_v3.services.projects.pagers import ListProjectsAsyncPager
+
 
 async def worker(name, folder_queue: asyncio.Queue, project_queue: asyncio.Queue):
     folder_client = resourcemanager.FoldersAsyncClient()
@@ -27,45 +29,33 @@ async def worker(name, folder_queue: asyncio.Queue, project_queue: asyncio.Queue
        
         folder_queue.task_done()
 
+        # queue.put_nowait(1.0)
+        # print(f'{name} has slept for {sleep_for:.2f} seconds')
+        print(f"====={name} end =======")
 
-bq_client = bigquery.Client()
-import itertools
-# https://googleapis.dev/python/bigquery/latest/generated/google.cloud.bigquery.dataset.DatasetListItem.html#google.cloud.bigquery.dataset.DatasetListItem
-# https://googleapis.dev/python/bigquery/latest/generated/google.cloud.bigquery.dataset.Dataset.html#google.cloud.bigquery.dataset.Dataset
-async def list_datasets(project: str = None):
-    dataset_list = []
-    for dataset_list_item in bq_client.list_datasets(project=project):
-        dataset_list.append(bq_client.get_dataset(dataset_list_item.reference))
-    return dataset_list
-
-
-async def print_datasets(project_list):
-    tasks = [asyncio.create_task(list_datasets(p["project_id"])) for p in project_list]
-    result = await asyncio.gather(*tasks)
-    print(list(itertools.chain.from_iterable(result)))
-
-async def main(organization_id: str):
+async def main():
     folder_queue = asyncio.Queue()
     project_queue = asyncio.Queue()
 
-    await folder_queue.put(organization_id)
+    await folder_queue.put("organizations/1045933413058")
     tasks = []
     for i in range(3):
         task = asyncio.create_task(worker(f'worker-{i}', folder_queue, project_queue))
         tasks.append(task)
-    
+
+    await folder_queue.join()
+  
     for task in tasks:
         task.cancel()
-    # すべてのワーカータスクがキャンセルされるまで待ちます。
     await asyncio.gather(*tasks, return_exceptions=True)
-    # await print_datasets(project_list=project_list)
 
-    dataset_tasks = [asyncio.create_task(list_datasets())]
+    # print(f'3 workers slept in parallel for {total_slept_for:.2f} seconds')
+    # print(f'total expected sleep time: {total_sleep_time:.2f} seconds')
 
 if __name__ == "__main__":
-    organization_id = "organizations/1045933413058"
-    started_at = time.monotonic()
-    asyncio.run(main())
-    total_slept_for = time.monotonic() - started_at
-    print(f'{total_slept_for:.2f} seconds')
-    
+  started_at = time.monotonic()
+  asyncio.run(main())
+  total_slept_for = time.monotonic() - started_at
+  print(f'{total_slept_for:.2f} seconds')
+  
+  
