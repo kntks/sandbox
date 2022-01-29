5066f5fda1d0a3a92c604ea4e337d7632cc1a422
kntks <0180takeshi.aefd@gmail.com>
2022-01-29 12:03:23 +0900

    add Number constraint

--- golang/sandbox/cmd/generics/main.go
+++ golang/sandbox/cmd/generics/main.go
@@ -22,9 +22,21 @@ func some[K constraints.Ordered](list []K, f func(K) bool) bool {
 	return false
 }
 
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+func sum2[K Number, T []K](list T) K {
+	return sum[K](list)
+}
+
 func main() {
 	fmt.Println(sum([]int{1, 2, 3}))
 	fmt.Println(sum([]string{"a", "b"}))
+
+	// error: string does not implement Number
+	// fmt.Println(sum2([]string{"a", "b"}))
+	fmt.Println(sum2([]float64{1.2, 3.4}))
 
 	fmt.Println(some([]int{1, 3, 4}, func(x int) bool { return x%2 == 0 }))
 }
