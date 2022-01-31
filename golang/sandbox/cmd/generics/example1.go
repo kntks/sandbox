682135c23d13b47dfe56931d6d18e775e9d61b70
kntks <0180takeshi.aefd@gmail.com>
2022-01-31 17:36:16 +0900

    update

--- golang/sandbox/cmd/generics/example1.go
+++ golang/sandbox/cmd/generics/example1.go
@@ -5,38 +5,20 @@ import (
 	"fmt"
 )
 
-func sum[K constraints.Ordered, T []K](m T) K {
+func Sum[K Number](list []K) K {
 	var sum K
-	for _, x := range m {
+	for _, x := range list {
 		sum += x
 	}
 	return sum
-}
-
-func some[K constraints.Ordered](list []K, f func(K) bool) bool {
-	for _, x := range list {
-		if f(x) {
-			return true
-		}
-	}
-	return false
 }
 
 type Number interface {
 	constraints.Integer | constraints.Float
 }
 
-func sum2[K Number, T []K](list T) K {
-	return sum[K](list)
-}
-
 func Example1() {
-	fmt.Println(sum([]int{1, 2, 3}))
-	fmt.Println(sum([]string{"a", "b"}))
+	fmt.Println(Sum([]int{1, 2, 3}))
 
-	// error: string does not implement Number
-	// fmt.Println(sum2([]string{"a", "b"}))
-	fmt.Println(sum2([]float64{1.2, 3.4}))
-
-	fmt.Println(some([]int{1, 3, 4}, func(x int) bool { return x%2 == 0 }))
+	fmt.Println(Sum([]float64{1.2, 3.4}))
 }
--- golang/sandbox/cmd/generics/main.go
+++ golang/sandbox/cmd/generics/main.go
@@ -77,6 +77,10 @@ func Map[K, T any](list []K, f func(K) T) []T {
 	return result
 }
 
+type stuff struct {
+	price int
+}
+
 func Example_Map() {
 	fmt.Println("=====  Map example1 ========")
 	input1 := []int{1, 2, 3, 4}
@@ -84,12 +88,13 @@ func Example_Map() {
 	fmt.Printf("input: %#v\n", input1)
 	fmt.Printf("output: %#v\n", output1)
 
-	type stuff struct {
-		price int
+	fmt.Println("=====  Map example2 ========")
+	input2 := []stuff{
+		{price: 10},
+		{price: 20},
+		{price: 30},
 	}
 
-	fmt.Println("=====  Map example2 ========")
-	input2 := []stuff{{price: 10}, {price: 20}, {price: 30}}
 	output2 := Map(input2, func(s stuff) stuff {
 		return stuff{price: int(math.Ceil(float64(s.price) * 1.5))}
 	})
@@ -97,13 +102,96 @@ func Example_Map() {
 	fmt.Printf("output: %+v\n", output2)
 }
 
+func Reduce[K, T any](list []K, f func(acc T, value K, index int) T, initialValue T) T {
+	acc := initialValue
+	for i, x := range list {
+		acc = f(acc, x, i)
+	}
+	return acc
+}
+
+func Example_Reduce() {
+	fmt.Println("=====  Reduce example1 ========")
+
+	input1 := []stuff{
+		{price: 10},
+		{price: 20},
+		{price: 30},
+	}
+
+	output1 := Reduce(input1, func(acc int, value stuff, index int) int {
+		return acc + value.price
+	}, 0)
+	fmt.Printf("input: %+v\n", input1)
+	fmt.Printf("output: %+v\n", output1)
+}
+
+func Every[K any](list []K, f func(K) bool) bool {
+	for _, x := range list {
+		if !f(x) {
+			return false
+		}
+	}
+	return true
+}
+
+func Some[K any](list []K, f func(K) bool) bool {
+	for _, x := range list {
+		if f(x) {
+			return true
+		}
+	}
+	return false
+}
+
+func Includes[K comparable](list []K, elem K) bool {
+	return Some(list, func(x K) bool { return x == elem })
+}
+
+func IncludesAll[K comparable](list []K, elems []K) bool {
+	return Every(elems, func(x K) bool { return Includes(list, x) })
+}
+
+var permissionList = []string{
+	"create", "read", "update", "delete",
+}
+
+type User struct {
+	permission []string
+}
+
+func (u *User) CanDelete() bool {
+	return Some(u.permission, func(x string) bool {
+		return x == "delete"
+	})
+}
+
+func (u *User) IsAdmin() bool {
+	return IncludesAll(u.permission, permissionList)
+}
+
+func Example_Some() {
+	user1 := User{
+		permission: []string{"read"},
+	}
+	fmt.Printf("person1 can delete xxx: %v\n", user1.CanDelete())
+	fmt.Printf("person1 is admin: %v\n", user1.IsAdmin())
+
+	user2 := User{
+		permission: []string{"create", "delete"},
+	}
+	fmt.Printf("person2 can delete xxx: %v\n", user2.CanDelete())
+	fmt.Printf("person2 is admin: %v\n", user2.IsAdmin())
+
+}
+
 func main() {
 	// Example1()
 	// Print(A{hoge: "AAA"})
 	// Print(B{hoge: "BBB"})
 
-	// Print1("a")
-	// Print1(1)
-	Example_Filter()
-	Example_Map()
+	// Example_Filter()
+	// Example_Map()
+	// Example_Reduce()
+	Example_Some()
 }
