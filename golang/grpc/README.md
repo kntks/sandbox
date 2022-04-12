542e2281d08176b43f767eb7fed4713831770f56
kntks <0180takeshi.aefd@gmail.com>
2022-04-12 23:15:39 +0900

    add example code

---
+++ golang/grpc/README.md
@@ -0,0 +1,9 @@
+# build
+```
+$ docker compose run --rm protobuf
+```
+
+memo
+```
+$ buf generate proto/v1/sample.proto
+```
--- golang/grpc/server/main.go
+++ golang/grpc/server/main.go
@@ -2,9 +2,69 @@ package main
 
 import (
 	"context"
+	"log"
+	"net"
+	"net/http"
+
+	pb "grpc/gen/go/proto/v1"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-func main() {
-	ctx, cancel := context.WithCancel(context.Background())
+type server struct {
+	pb.UnimplementedHogeServer
+}
 
+func NewServer() *server {
+	return &server{}
+}
+
+func (s *server) AtoB(ctx context.Context, in *pb.AA) (*pb.BB, error) {
+	return &pb.BB{Name: "hello world"}, nil
+}
+
+func main() {
+	// Create a listener on TCP port
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("Failed to listen:", err)
+	}
+
+	// Create a gRPC server object
+	s := grpc.NewServer()
+	// Attach the Greeter service to the server
+	pb.RegisterHogeServer(s, &server{})
+	// Serve gRPC server
+	log.Println("Serving gRPC on 0.0.0.0:8080")
+	go func() {
+		log.Fatalln(s.Serve(lis))
+	}()
+	// Create a client connection to the gRPC server we just started
+	// This is where the gRPC-Gateway proxies the requests
+	conn, err := grpc.DialContext(
+		context.Background(),
+		"0.0.0.0:8080",
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	// Register Greeter
+	err = pb.RegisterHogeHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    ":8090",
+		Handler: gwmux,
+	}
+
+	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
+	log.Fatalln(gwServer.ListenAndServe())
 }
