af72a51c28a056494b2471cd4dff5b0f8969aaff
kntks <0180takeshi.aefd@gmail.com>
2022-04-19 23:06:49 +0900

    add example generator

--- golang/sandbox/cmd/concurrent/main.go
+++ golang/sandbox/cmd/concurrent/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sandbox/concurrency"
 	"time"
@@ -8,8 +9,30 @@ import (
 
 func main() {
 	// concurrency.Case3()
-	concurrency.LockMap()
-	PipelineMain()
+	// concurrency.LockMap()
+	// PipelineMain()
+	GeneratorMain()
+}
+
+func GeneratorMain() {
+	for v := range generators(context.Background()) {
+		fmt.Println("処理中", v)
+	}
+}
+func generators(ctx context.Context) <-chan int {
+	ch := make(chan int)
+	go func() {
+		defer close(ch)
+		for i := 0; i < 5; i++ {
+			select {
+			case <-ctx.Done():
+				return
+			case ch <- i:
+				time.Sleep(time.Duration(i) * time.Second)
+			}
+		}
+	}()
+	return ch
 }
 
 func PipelineMain() {
--- golang/sandbox/go.mod
+++ golang/sandbox/go.mod
@@ -1,6 +1,6 @@
 module sandbox
 
-go 1.16
+go 1.18
 
 require (
 	cloud.google.com/go/resourcemanager v1.0.0
