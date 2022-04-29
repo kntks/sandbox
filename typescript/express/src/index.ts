dd6cf6cfa280657616f6b82eccb1a10c5a5186bc
kntks <0180takeshi.aefd@gmail.com>
2022-04-30 00:08:36 +0900

    add: index.ts

--- typescript/express/src/index.ts
+++ typescript/express/src/index.ts
@@ -3,42 +3,30 @@ const app: express.Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//CROS対応（というか完全無防備：本番環境ではだめ絶対）
-app.use(
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    next();
-  }
-);
+// app.use(
+//   (req: express.Request, res: express.Response, next: express.NextFunction) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "*");
+//     res.header("Access-Control-Allow-Headers", "*");
+//     next();
+//   }
+// );
 
 app.listen(3000, () => {
   console.log("Start on port 3000.");
 });
 
-type User = {
-  id: number;
-  name: string;
-  email: string;
-};
-
-const users: User[] = [
-  { id: 1, name: "User1", email: "user1@test.local" },
-  { id: 2, name: "User2", email: "user2@test.local" },
-  { id: 3, name: "User3", email: "user3@test.local" },
-];
-
-//一覧取得
-app.get("/users", (req: express.Request, res: express.Response) => {
-  res.send(JSON.stringify(users));
-});
-
 import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
-app.get("/", async (req: express.Request, res: express.Response) => {
-  const prisma = new PrismaClient();
+app.get("/users", async (req: express.Request, res: express.Response) => {
   const allUsers = await prisma.user.findMany()
   console.log(allUsers)
   res.send(JSON.stringify(allUsers))
+})
+
+app.post("/create", async (req: express.Request, res: express.Response) => {
+  console.log("req.body",req.body);
+  const user = await prisma.user.create({data: req.body});
+  res.send(JSON.stringify(user));
 })
