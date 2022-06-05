52fe2db40415eb37bda646e9ceec83e0ed304f28
kntks <0180takeshi.aefd@gmail.com>
2022-06-05 15:58:51 +0900

    fix: const

--- typescript/express/src/const.ts
+++ typescript/express/src/const.ts
@@ -1,42 +1,40 @@
 export const departments = [
   {
-    dept_no: "d001",
-    dept_name: "marketing",
+    deptNo: "d001",
+    deptName: "marketing",
   },
   {
-    dept_no: "d002",
-    dept_name: "finance",
+    deptNo: "d002",
+    deptName: "finance",
   },
   {
-    dept_no: "d003",
-    dept_name: "human_resources",
+    deptNo: "d003",
+    deptName: "human_resources",
   },
   {
-    dept_no: "d004",
-    dept_name: "production",
+    deptNo: "d004",
+    deptName: "production",
   },
   {
-    dept_no: "d005",
-    dept_name: "development",
+    deptNo: "d005",
+    deptName: "development",
   },
   {
-    dept_no: "d006",
-    dept_name: "quality_management",
+    deptNo: "d006",
+    deptName: "quality_management",
   },
   {
-    dept_no: "d007",
-    dept_name: "sales",
+    deptNo: "d007",
+    deptName: "sales",
   },
   {
-    dept_no: "d008",
-    dept_name: "research",
+    deptNo: "d008",
+    deptName: "research",
   },
   {
-    dept_no: "d009",
-    dept_name: "customer_service",
+    deptNo: "d009",
+    deptName: "customer_service",
   },
 ] as const;
 
-type Department = typeof departments[number]["dept_name"];
-
-export const DepartmentNames = departments.map((v) => v.dept_name);
+export const DepartmentNames = departments.map(({deptName})=> deptName)
